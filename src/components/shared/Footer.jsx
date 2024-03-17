3ce492186cc0678018a6f92f32916ec7a62c6e74
AlamRazek <ohidul933@gmail.com>
2024-03-17 18:15:10 +0000

    Your commit message here

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { Container } from "postcss";
+
+const Footer = () => {
+  return (
+    <Box className="bg-black px-2 py-10">
+      <Container></Container>
+    </Box>
+  );
+};
+
+export default Footer;
